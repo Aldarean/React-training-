48ec3e0b0922be9cb8041d1d74b689dc43854d68
Rodney Wormsbecher <rodney22@live.nl>
2019-03-13 20:55:15 +0100

    components - start

--- team-card/src/App.js
+++ team-card/src/App.js
@@ -1,27 +1,28 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
-class App extends Component {
+class App extends React.Component { 
   render() {
+    var isLoading = false;
+    var data = {
+      name: "Rodney",
+      age: 32,
+      job: 'Software developer'
+    }
+
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <p>
+          Is the component loading? 
+          {isLoading ? 'yes' : 'No'}
+        </p>
+        <p>
+          Hi my name is {data.name}, 
+          I'm {data.age} years old 
+          and I'm employed as a {data.job}
+        </p>
+
       </div>
-    );
+    )
   }
 }
 
